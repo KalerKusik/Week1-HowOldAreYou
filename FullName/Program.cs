4bdd63bac23d50721f967207440c60630b042969
KalerKusik <karel.kuusik1@gmail.com>
2021-09-01 12:18:34 +0300

    Read Inputs

---
+++ FullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja käest eesnime
+            //programm küsib kasutaja käest perekonnanime
+            //programm tervitab kasutajat kasutades neid andmeid
+
+
+
+            Console.WriteLine("What is your first name?");
+            string UserFirstName = Console.ReadLine();
+
+            Console.WriteLine("And what is your name?");
+            string UserLastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {UserFirstName} {UserLastName}");
+        }
+    }
+}
---
+++ HowOldAreYou/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HowOldAreYOu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine($"Hello,{UserName}");
+
+            Console.WriteLine("How old are you? Enter digits only:");
+
+            int Age = Int32.Parse(Console.ReadLine());
+
+            int YearOfBirth = 2021 - Age;
+
+            Console.WriteLine($"You were born in {YearOfBirth}");
+        }
+    }
+}
